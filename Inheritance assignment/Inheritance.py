79b2d40fef19f57fbd9e52c92a03e83a8a45fb00
Blake Berry <blakeberry85@yahoo.com>
2020-06-08 18:00:08 -0700

    Create Inheritance.py

---
+++ Inheritance assignment/Inheritance.py
@@ -0,0 +1,17 @@
+
+
+class property:
+    address = 'Not listed'
+    price = 'N/A'
+    rooms = 'N/A'
+    sqft = 'N/A'
+
+class house(property):
+    floors = 'N/A'
+    neighborhood = 'Not listed'
+
+class apartment(property):
+    aptnumber = 'N/A'
+    aptcomplex = 'Not listed'
+    
+    
