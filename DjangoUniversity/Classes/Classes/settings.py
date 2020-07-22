6e2540cfb347b8855d28489249d27b0286038967
Blake Berry <blakeberry85@yahoo.com>
2020-07-22 07:48:21 -0700

    Revert "Update settings.py"
    This reverts commit a16316851dcbb9f49e4659398c21eb489c02600c.

--- DjangoUniversity/Classes/Classes/settings.py
+++ DjangoUniversity/Classes/Classes/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'p_g6ncd0x9ojo(oo6wlxt=(o_b1*xk)oac3$3)em^u39ci&xz-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
